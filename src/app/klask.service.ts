5ad1c3bbdfa099f1d0bfee0d9fbfd1ac3ed209e8
Tom Steele <tsteele@madisoncollege.edu>
2020-03-12 20:14:12 -0500

    Improved dummy data...
    Still needs data for rest of tourney ! ! !

--- src/app/klask.service.ts
+++ src/app/klask.service.ts
@@ -10,38 +10,27 @@ export class KlaskService {
   getTournamentGameResults(tourneyId: string) {
     return [
       { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent: "Valeria" }
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	klask	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	score	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom	biscuit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom	score	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom	score	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	biscuit	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	score	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	loss-control	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria	score	Tom
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor	forfeit-Valeria	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria
-      // { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom	forfeit	Valeria      
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"klask", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"score", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "biscuit", opponent: "Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", pointType: "score", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"biscuit", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"score", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"loss-control", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", pointType:	"score", opponent:	"Tom" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }
+      , { tourneyId: "some guid", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", pointType:	"forfeit", opponent:	"Valeria" }      
     ];
   }
 
