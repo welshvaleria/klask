48e35ac52cf3416f9d575fd00253ad83fa4d10a8
Tom Steele <tsteele@madisoncollege.edu>
2020-03-13 08:19:52 -0500

    Grouped into object...
    Need to learn some more of this... Or not, and let my students learn it for me : - )

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,6 +14,17 @@ export class HomePage {
     console.log(this.klaskSvc);
     const foo = this.klaskSvc.getTournamentGameResults("some guid");
     console.log(foo);
-  }
 
+    const groupByGameNumber = foo.reduce(
+      (acc, x) => {
+        acc[x.gameNumber] 
+          ? acc[x.gameNumber] = [...acc[x.gameNumber], x] 
+          : acc[x.gameNumber] = [x];
+        return acc;
+      }
+      , {}
+    );
+
+    console.log(groupByGameNumber);
+  }
 }
