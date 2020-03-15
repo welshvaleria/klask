9ba153a10c3e1b88ba4dc44490d992367df49dbe
Tom Steele <tsteele@madisoncollege.edu>
2020-03-15 11:43:29 -0500

    Better formatted JSON...
    And exprimental, wait for it...
    
    Partition Sort Attribute Modeling Interactive (PSAMI)
    
    ...in Excel...
    
    I'm trying to invent this notation as a NoSQL modeling notation. Dellusions of granduer for sure : - )

--- src/app/klask.service.ts
+++ src/app/klask.service.ts
@@ -9,28 +9,215 @@ export class KlaskService {
 
   getTournamentGameResults(tourneyId: string) {
     return [
-      { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 1, scorerPointNumber: 1, pointType: "score", opponent: "Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 2, scorerPointNumber: 1, pointType:	"klask", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 3, scorerPointNumber: 2, pointType:	"score", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 4, scorerPointNumber: 2, pointType: "biscuit", opponent: "Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 5, scorerPointNumber: 3, pointType: "score", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 6, scorerPointNumber: 4, pointType: "score", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 7, scorerPointNumber: 3, pointType:	"biscuit", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 8, scorerPointNumber: 4, pointType:	"score", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 9, scorerPointNumber: 5, pointType:	"loss-control", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	1, pointDateTime: "some datetime", scorer: "Valeria", gamePointNumber: 10, scorerPointNumber: 6, pointType:	"score", opponent:	"Tom" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 1, scorerPointNumber: 1, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 2, scorerPointNumber: 2, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 3, scorerPointNumber: 3, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 4, scorerPointNumber: 4, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 5, scorerPointNumber: 5, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	2, pointDateTime: "some datetime", scorer: "Trevor", gamePointNumber: 6, scorerPointNumber: 6, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 1, scorerPointNumber: 1, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 2, scorerPointNumber: 2, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 3, scorerPointNumber: 3, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 4, scorerPointNumber: 4, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 5, scorerPointNumber: 5, pointType:	"forfeit", opponent:	"Valeria" }
-      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4", tourneyName: 	"mita", gameNumber:	3, pointDateTime: "some datetime", scorer: "Tom", gamePointNumber: 6, scorerPointNumber: 6, pointType:	"forfeit", opponent:	"Valeria" }      
+        { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+          , tourneyName: 	"mita"
+          , gameNumber:	1
+          , pointDateTime: "some datetime"
+          , scorer: "Tom"
+          , gamePointNumber: 1
+          , scorerPointNumber: 1
+          , pointType: "score"
+          , opponent: "Valeria" 
+        }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 2
+        , scorerPointNumber: 1
+        , pointType:	"klask"
+        , opponent:	"Tom" }
+  
+        , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 3
+        , scorerPointNumber: 2
+        , pointType:	"score"
+        , opponent:	"Tom" }
+  
+        , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 4
+        , scorerPointNumber: 2
+        , pointType: "biscuit"
+        , opponent: "Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 5
+        , scorerPointNumber: 3
+        , pointType: "score"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 6
+        , scorerPointNumber: 4
+        , pointType: "score"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 7
+        , scorerPointNumber: 3
+        , pointType:	"biscuit"
+        , opponent:	"Tom" 
+      }
+
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 8
+        , scorerPointNumber: 4
+        , pointType:	"score"
+        , opponent:	"Tom" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 9
+        , scorerPointNumber: 5
+        , pointType:	"loss-control"
+        , opponent:	"Tom" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	1
+        , pointDateTime: "some datetime"
+        , scorer: "Valeria", gamePointNumber: 10
+        , scorerPointNumber: 6
+        , pointType:	"score"
+        , opponent:	"Tom" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 1
+        , scorerPointNumber: 1
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 2
+        , scorerPointNumber: 2
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 3
+        , scorerPointNumber: 3
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 4
+        , scorerPointNumber: 4
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 5
+        , scorerPointNumber: 5
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	2
+        , pointDateTime: "some datetime"
+        , scorer: "Trevor", gamePointNumber: 6
+        , scorerPointNumber: 6
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 1
+        , scorerPointNumber: 1
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 2
+        , scorerPointNumber: 2
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 3
+        , scorerPointNumber: 3
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 4
+        , scorerPointNumber: 4
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 5
+        , scorerPointNumber: 5
+        , pointType:	"forfeit"
+        , opponent:	"Valeria" 
+      }
+      , { tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+        , tourneyName: 	"mita"
+        , gameNumber:	3
+        , pointDateTime: "some datetime"
+        , scorer: "Tom"
+        , gamePointNumber: 6
+        , scorerPointNumber: 6
+        , pointType:	"forfeit"
+        , opponent:	"Valeria"
+      }
     ];
   }
 
