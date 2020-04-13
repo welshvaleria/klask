5f01105f4b0e60aed2c3247cd245d5abfb6aa00d
Tom Steele <tsteele@madisoncollege.edu>
2020-04-13 16:12:06 -0500

    This works...
    But pretty complex...
    
    But it is what/how I would mentor junior devs up on these techniques ! ! !

--- src/app/win-loss-stats/win-loss-stats.page.ts
+++ src/app/win-loss-stats/win-loss-stats.page.ts
@@ -13,9 +13,28 @@ export class WinLossStatsPage implements OnInit {
   results: any[];
 
   ngOnInit() {
-    this.results = this.klaskSvc.getTournamentGameResults("1073ed04-45ef-444e-8263-8cc77b5251e4");
-    console.log(this.results);
+    const foo = this.klaskSvc.getTournamentGameResults("1073ed04-45ef-444e-8263-8cc77b5251e4");
 
     // Practice filtering... reducing... ??? Here...
+    const bar = foo.reduce(
+      (acc, x) => {
+        
+        acc.set(x.winner, { 
+          wins: acc.get(x.winner) ? acc.get(x.winner).wins + 1 : 1
+          , losses: acc.get(x.winner) ? acc.get(x.winner).losses : 0
+        });
+        
+        acc.set(x.loser, { 
+          wins: acc.get(x.loser) ? acc.get(x.loser).wins : 0
+          , losses: acc.get(x.loser) ? acc.get(x.loser).losses + 1 : 1
+        });
+
+        return acc;
+      }
+      , new Map()
+    );
+
+    console.log(bar);
+
   }
 }
