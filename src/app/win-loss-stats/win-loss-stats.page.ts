a5b0c5a8205c3f3984528d9fbeae1ff7818dcf36
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-13 17:19:12 -0500

    Getting wins/losses for each player
    Have the wins/losses pulled, but need to find a way to make the data more workable for within the html file. Will try a spread operator in next commit.

--- src/app/win-loss-stats/win-loss-stats.page.ts
+++ src/app/win-loss-stats/win-loss-stats.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { KlaskService } from '../klask.service';
+import { access } from 'fs';
 
 @Component({
   selector: 'app-win-loss-stats',
@@ -16,6 +17,33 @@ export class WinLossStatsPage implements OnInit {
     this.results = this.klaskSvc.getTournamentGameResults("1073ed04-45ef-444e-8263-8cc77b5251e4");
     console.log(this.results);
 
-    // Practice filtering... reducing... ??? Here...
+    // Reducing data to get wins/losses per player
+    const winsLosses = this.results.reduce(
+      (acc, x) => {
+
+        // Setting the wins for each player
+        acc.set(x.winner, {
+          // If the person has won before, add one. Else it's just 1
+          wins: acc.get(x.winner) ? acc.get(x.winner).wins + 1 : 1
+          //Losses will always stay at 0 here.
+          , losses: acc.get(x.winner) ? acc.get(x.winner).losses : 0
+        });
+
+        // Setting the losses for each player.
+        acc.set(x.loser, {
+          //Wins will always stay at what was previously set here
+          wins: acc.get(x.loser) ? acc.get(x.loser).wins : 0
+          // If the person has lost before, add one. Else it's just 1
+          , losses: acc.get(x.loser) ? acc.get(x.loser).losses + 1 : 1
+        });
+
+        return acc;
+      }
+      , new Map()
+    );
+
+    // Playing around with how to get data out of the Map that is returned.
+    console.log(winsLosses);
+    winsLosses.forEach(element => console.log(`Wins: ${element.wins} Losses: ${element.losses} Win%: ${element.wins / (element.wins + element.losses)}`));
   }
 }
