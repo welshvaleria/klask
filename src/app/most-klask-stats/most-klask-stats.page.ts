b8ccfc326b92a1be6195cc86930d71d0bc695dad
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-16 18:05:19 -0500

    Times klasked successfully extracted
    Each user has their number of klasks counted and will be displayed on this webpage

--- src/app/most-klask-stats/most-klask-stats.page.ts
+++ src/app/most-klask-stats/most-klask-stats.page.ts
@@ -11,23 +11,35 @@ export class MostKlaskStatsPage implements OnInit {
   constructor(private klaskSvc: KlaskService) { }
 
   results: any[];
+  klaskArray: any[];
 
   ngOnInit() {
     this.results = this.klaskSvc.getTournamentGameResults("1073ed04-45ef-444e-8263-8cc77b5251e4");
+    // console.log(this.results);
 
-    const pointPuller = this.results.reduce((acc, x) => {
-        // const pointTypeDistribution = x.points.reduce(
-        //   (acc2, x2) => {
-        //     if (x2.pointType == "klask") {
-        //       // console.log(`Klasker: ${x2.opponent} Game No: ${x.gameNumber} Game Point No: ${x2.gamePointNumber}`);
-        //       acc.set(x2.opponent, {
-        //         klasks: acc2.get(x2.opponent) ? acc2.get(x2.opponent).klasks + 1 : 1
-        //       });
-        //     }
-        //     return pointTypeDistribution
-        // })
-    }, 0);
-    console.log(pointPuller);
+    const allPointsScored = this.results.reduce((acc, x) => {
+      return acc.concat(x.points);
+    }, []);
+
+    const klaskPoints = allPointsScored.filter(point => point.pointType === "klask");
+
+    const playersWhoKlasked = klaskPoints.map(klasker => klasker.opponent);
+
+    const timesKlasked = playersWhoKlasked.reduce((acc, x) => {
+
+      acc.set(x, {
+        klasks: acc.get(x) ? acc.get(x).klasks + 1 : 1
+      });
+      return acc;
+
+    }, new Map());
+
+    // console.log(timesKlasked);
+
+    this.klaskArray = [...timesKlasked].map(x => ({
+      name: x[0]
+      , klaskOffenses: x[1].klasks
+    }));
   }
 
 }
