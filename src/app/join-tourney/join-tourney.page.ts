7f64ef2484797028ece08681a04600119c751a73
welshvaleria <valeriawelsh@hotmaail.com>
2020-04-17 14:14:35 -0500

    cleaned the code

--- src/app/join-tourney/join-tourney.page.ts
+++ src/app/join-tourney/join-tourney.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { KlaskService } from '../klask.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,21 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class JoinTourneyPage implements OnInit {
 
-  constructor(private klaskSvc: KlaskService, private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
-  tournaments = [];
   tourneyName = null;
   username = "";
 
   ngOnInit() {
-    this.tournaments = this.klaskSvc.getTournaments();
     this.tourneyName = this.activatedRoute.snapshot.paramMap.get("tourneyName");
-    // this.klaskSvc[1][0].tourneyName;
-    // alert(this.klaskSvc[1][0].tourneyName);
   }
 
   test() {
-      alert(this.tourneyName);
       alert(this.username);
   }
 
