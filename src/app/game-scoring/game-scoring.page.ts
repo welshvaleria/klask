e99d79d9fbffad07f8e02ad5163e10e83376d737
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-19 17:11:21 -0500

    Ionic Action Sheet example to maybe use...
    This one seems more intuitive to me, honestly.

--- src/app/game-scoring/game-scoring.page.ts
+++ src/app/game-scoring/game-scoring.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PickerController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-game-scoring',
@@ -8,10 +8,36 @@ import { PickerController } from '@ionic/angular';
 })
 export class GameScoringPage implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetController : ActionSheetController) { }
+
+  async presentActionSheet() {
+    await this.actionSheetController.create({
+      header: 'Points'
+      , buttons: [{
+          text: "Klasked"
+          , handler: () => {
+              console.log("This person Klasked");
+          }
+      }, {
+          text: "Loss of Control"
+          , handler: () => {
+              console.log("This person lost control");
+          }
+      }, {
+          text: "Normal point"
+          , handler: () => {
+              console.log("Normal point scored");
+          }
+      }, {
+          text: "Biscuits"
+          , handler: () => {
+              console.log("Biscuit point");
+          }
+      }]
+    }).then(res => res.present());
+  }
 
   ngOnInit() {
-    window.PickerController = PickerController;
   }
 
 }
