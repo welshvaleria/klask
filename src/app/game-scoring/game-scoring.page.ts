01fd6242b3d6c0235941c255bc8028c6c30d8889
Tom Steele <tsteele@madisoncollege.edu>
2020-04-22 18:52:18 -0500

    Meh...

--- src/app/game-scoring/game-scoring.page.ts
+++ src/app/game-scoring/game-scoring.page.ts
@@ -16,29 +16,35 @@ export class GameScoringPage implements OnInit {
 	let scoreTime = Date.now();
 	
     await this.actionSheetController.create({
-      header: 'Points'
+      header: 'What happened...'
+      //, subHeader: "Foo"
       , buttons: [{
-          text: "Klasked"
+          text: "Klask (opponent Klasked)"
           , handler: () => {
             console.log("Klasked");
             this.scores[scoreIndex] += 1
           }
       }, {
-          text: "Loss of Control"
+          text: "Loss of Control (by opponent)"
           , handler: () => {
               	console.log("This person lost control");
           }
       }, {
-          text: "Normal point"
+          text: "Score (I scored)"
           , handler: () => {
               	console.log("Normal point scored");
           }
       }, {
-          text: "Biscuits"
+          text: "Biscuit (opponent collected 3 biscuits)"
           , handler: () => {
               	console.log("Biscuit point");
           }
-      }, {
+      }
+      , {
+        text: "-1 (Oops, correcting a mistake)"
+        , handler: () => this.scores[scoreIndex] -= 1
+      }
+      , {
           text: "Cancel"
           , icon: "close"
           , role: "cancel"
