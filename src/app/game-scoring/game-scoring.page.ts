ec8776a9a98f9086476685fd2d2fc109b0f48134
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-22 18:18:06 -0500

    Adding backend functionality, ran into roadblock trying to get which button was clicked
    ActionSheet knowledge needs to be gained...

--- src/app/game-scoring/game-scoring.page.ts
+++ src/app/game-scoring/game-scoring.page.ts
@@ -6,46 +6,55 @@ import { ActionSheetController } from '@ionic/angular';
   templateUrl: './game-scoring.page.html',
   styleUrls: ['./game-scoring.page.scss'],
 })
+
 export class GameScoringPage implements OnInit {
 
-	animals: string[] = ["Tiger", "Lion", "Bear", "Elephant"];
   constructor(private actionSheetController : ActionSheetController) { }
 
-  async presentActionSheet() {
+  async presentActionSheet(test) {
+
+	let player1 = document.querySelector("#player1");
+	let player2 = document.querySelector("#player1");
+	let scoreTime = Date.now();
+	
     await this.actionSheetController.create({
       header: 'Points'
       , buttons: [{
           text: "Klasked"
           , handler: () => {
-              console.log("This person Klasked");
+				  console.log(test);
+				  // TODO: How to get which button was clicked?
+				player1.innerHTML = (parseInt(document.querySelector("#player1").innerHTML, 10) + 1).toString();
           }
       }, {
           text: "Loss of Control"
           , handler: () => {
-              console.log("This person lost control");
+              	console.log("This person lost control");
           }
       }, {
           text: "Normal point"
           , handler: () => {
-              console.log("Normal point scored");
+              	console.log("Normal point scored");
           }
       }, {
           text: "Biscuits"
           , handler: () => {
-              console.log("Biscuit point");
+              	console.log("Biscuit point");
           }
       }, {
           text: "Cancel"
           , icon: "close"
           , role: "cancel"
           , handler: () => {
-            console.log("Cancelled");
+            	console.log("Cancelled");
           }
       }]
     }).then(res => res.present());
-  }
+  } 
 
   ngOnInit() {
+	  document.querySelector("#player1").innerHTML = "0";
+	  document.querySelector("#player2").innerHTML = "0";
   }
 
 }
