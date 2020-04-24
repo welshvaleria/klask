ed4a5e12a4803011761c617def1a78f8296b080e
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-23 21:45:04 -0500

    Points array progress, excited to show tomorrow.
    Still have some work to do, but I think the majority of that work is done now. Next is trying to check for the winner of the game.

--- src/app/game-scoring/game-scoring.page.ts
+++ src/app/game-scoring/game-scoring.page.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
+import { GameScoringPageRoutingModule } from './game-scoring-routing.module';
+
+interface currentPointData {
+	pointDateTime: string;
+	scorer: string;
+	gamePointNumber: number;
+	scorerPointNumber: number;
+	pointType: string;
+	opponent: string;
+
+}
 
 @Component({
   selector: 'app-game-scoring',
@@ -13,41 +24,47 @@ export class GameScoringPage implements OnInit {
 
   async presentActionSheet(scoreIndex) {
 
-	let testArray = [{
-		tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
-		, tourneyName: "trevorTest"
-        , gameNumber: 1 
-        , winner: ""
-		, loser: "" 
-		, points: []
-	}];
+	// let testArray = [{
+	// 	tourneyId: "1073ed04-45ef-444e-8263-8cc77b5251e4"
+	// 	, tourneyName: "trevorTest"
+    //     , gameNumber: 1 
+    //     , winner: ""
+	// 	, loser: "" 
+	// 	, points: []
+	// }];
 	
     await this.actionSheetController.create({
-      header: 'Points'
+      header: 'Type of Point Scored'
       , buttons: [{
           text: "Klasked"
           , handler: () => {
-            this.addScoreAndUpdateGameData(scoreIndex, "klask");
+			this.addScoreAndUpdateGameData(scoreIndex, "klask");
+			// console.log(this.scores[scoreIndex]["scorer"]);
+			// this.checkForWinner();
           }
       }, {
           text: "Loss of Control"
           , handler: () => {
 				this.addScoreAndUpdateGameData(scoreIndex, "loss of control");
+				// this.checkForWinner();
           }
       }, {
           text: "Normal point"
           , handler: () => {
 				this.addScoreAndUpdateGameData(scoreIndex, "score");
+				// this.checkForWinner();
           }
       }, {
           text: "Biscuits"
           , handler: () => {
 				this.addScoreAndUpdateGameData(scoreIndex, "biscuit");
+				// this.checkForWinner();
           }
       }, {
 		  text: "-1 (Correction)"
 		  , handler: () => {
-			  this.removeAPoint(scoreIndex);
+			  this.scores.splice(this.scores.indexOf(scoreIndex), 1);
+			  console.log(this.scores);
 		  } 
 	  }, { 
           text: "Cancel"
@@ -62,30 +79,36 @@ export class GameScoringPage implements OnInit {
 
   addScoreAndUpdateGameData(index, pointType) {
 
-    this.scores[index] += 1;
+	this.scores = [...this.scores, {
+		pointDateTime: Date.now().toString()
+		, scorer: index == 0 ? "Trevor" : "Valeria"
+		, opponent: index == 1 ? "Trevor" : "Valeria"
+		, gamePointNumber: this.scores.length + 1
+		, scorerPointNumber: index == 0 ? this.playerOneScore + 1 : this.playerTwoScore + 1 
+		, pointType: pointType
+	}];
 
-    let currGameData = {
-      pointDateTime: Date.now()
-      , scorer: "Trevor"
-      , gamePointNumber: this.scores.reduce((acc, x) => acc + x, 0)
-      , scorerPointNumber: function() {
-          this.scores[index]
-		}
-	  , pointType: pointType
-      , opponent: "Valeria"
-    };
-
-    console.log(currGameData);
+    console.log(this.scores);
   }
 
-  removeAPoint(index) {
-	this.scores[index] -= 1;
-  }
+//   checkForWinner() {
+// 	  if (this.scores[0]["scorerPointNumber"] == 6) {
+// 		console.log(`Game over. ${this.scores[0]["scorer"]} wins.`);
+// 	  }
+//   }
 
   ngOnInit() {
   }
 
-  scores = [0, 0]
+  scores: currentPointData[] = [];
+
+  get playerOneScore() {
+	  return this.scores.filter(x => x.scorer == "Trevor").length;
+  }
+
+  get playerTwoScore() {
+	return this.scores.filter(x => x.scorer == "Valeria").length;
+  }
 
 }
 
