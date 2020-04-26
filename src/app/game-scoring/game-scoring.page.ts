43005b44b6eea67f5cf75c5df00415a3404f3fbf
Trevor Moldenhauer <ttmoldenhauer@madisoncollege.edu>
2020-04-26 11:55:06 -0500

    Light cleanup

--- src/app/game-scoring/game-scoring.page.ts
+++ src/app/game-scoring/game-scoring.page.ts
@@ -73,6 +73,7 @@ export class GameScoringPage implements OnInit {
 			, pointType: "forfeit"
 		}];
 	}
+	console.log(this.scores);
   }
 
   async presentActionSheet(scoreIndex) {
@@ -124,8 +125,6 @@ export class GameScoringPage implements OnInit {
   addScoreAndUpdateGameData(index, pointType) {
 
 	this.isGameOver = ((index == 0 ? this.playerOneScore + 1 : this.playerTwoScore + 1) == 6 ? true : false);
-	let playerGetterForNow = document.querySelectorAll(".player-name");
-	let playerNames = Array.from(playerGetterForNow).map(x => x.innerHTML.trim());
 
 	this.scores = [...this.scores, {
 		pointDateTime: Date.now().toString()
